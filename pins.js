4b84cc1567b0f2fc44df31638c595cd4111d6cf2
Emmyjosh65 <ge5853987@gmail.com>
2026-09-08 20:03:55 +0100

    Create pins.js

---
+++ pins.js
@@ -0,0 +1,82 @@
+"use strict";
+
+
+/*
+  ABBADONS BAN PORTAL
+  Central configuration.
+*/
+
+
+window.ABBADONS_PINS = {
+
+  name:
+    "ABBADONS BAN PORTAL",
+
+
+  theme:
+    "gold",
+
+
+  owner:
+    "dannyisnowdylan",
+
+
+  ownerId:
+    "8097740602",
+
+
+  payment:
+    "8062285862",
+
+
+  telegram: {
+
+    channel:
+      "https://t.me/dannys_domainn",
+
+    group:
+      "https://t.me/+IzSZkM3PphpkMDY8",
+
+    owner:
+      "https://t.me/dannyisnowdylan"
+
+  },
+
+
+  videos: {
+
+    mainMenu:
+      "https://videotourl.com/videos/1788889467352-2b459df8-0016-4ff4-867d-8ed9450a66f7.mp4",
+
+    banSimulation:
+      "https://videotourl.com/videos/1788888913438-a1b8a599-08f4-4a91-bf18-a848b15e20d8.mp4"
+
+  },
+
+
+  methods: {
+
+    ban1: {
+
+      name:
+        "BAN1",
+
+      status:
+        "SPAM BAN"
+
+    },
+
+
+    ban2: {
+
+      name:
+        "BAN2",
+
+      status:
+        "SPAM BAN"
+
+    }
+
+  }
+
+};
